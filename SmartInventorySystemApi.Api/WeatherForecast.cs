57976e27705930827f3faa395c08b8ccea48cba7
shchoholiev <sh.codework@gmail.com>
2023-10-14 17:11:10 +0000

    Test

--- SmartInventorySystemApi.Api/WeatherForecast.cs
+++ SmartInventorySystemApi.Api/WeatherForecast.cs
@@ -4,7 +4,7 @@ public class WeatherForecast
 {
     public DateOnly Date { get; set; }
 
-    public int TemperatureC { get; set; }
+    public int TemperatureC { get; set; } = 0;
 
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
