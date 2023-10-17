14228e7fbb6b026f7f872f5b9e781ea307c6bde2
shchoholiev <sh.codework@gmail.com>
2023-10-17 02:57:31 +0000

    Added mapping

--- SmartInventorySystemApi.Api/Program.cs
+++ SmartInventorySystemApi.Api/Program.cs
@@ -1,3 +1,5 @@
+using SmartInventorySystemApi.Application;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,6 +10,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+builder.Services.AddMapper();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
