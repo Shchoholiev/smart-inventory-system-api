31719d1676986a0fe30c958e37d57563fabb5748
shchoholiev <sh.codework@gmail.com>
2023-10-21 03:12:19 +0000

    Added devices service interface

---
+++ SmartInventorySystemApi.Application/Models/Dto/DeviceDto.cs
@@ -0,0 +1,14 @@
+using SmartInventorySystemApi.Domain.Enums;
+
+namespace SmartInventorySystemApi.Application.Models.Dto;
+
+public class DeviceDto
+{
+    public string Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public DeviceType Type { get; set; }
+
+    public string GroupId { get; set; }
+}
