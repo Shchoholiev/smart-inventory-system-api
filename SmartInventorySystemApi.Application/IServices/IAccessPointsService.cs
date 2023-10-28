057b4f524393214150ccae23ad45fb36018aa778
shchoholiev <sh.codework@gmail.com>
2023-10-28 01:58:49 +0000

    Added Access Points Service interface

---
+++ SmartInventorySystemApi.Application/IServices/IAccessPointsService.cs
@@ -0,0 +1,17 @@
+namespace SmartInventorySystemApi.Application.IServices;
+
+public interface IAccessPointsService
+{
+    /// <summary>
+    /// <list type="number">
+    ///     <item>Reads QR/Bar code from an image using internal ML API. If found skip to #3.</item>
+    ///     <item>Recognizes an item on the image and searchers for it in a database. If found proceedes to #3.</item>
+    ///     <item>Turns on light on for the shelf where this item is located. </item>
+    /// </list>
+    /// All scans history is saved in a database.
+    /// Used only by <b>Access Point Device</b>.
+    /// </summary>
+    /// <param name="deviceGuid">Device Guid -> Azure IoT DeviceId</param>
+    /// <returns></returns>
+    Task FindItemByImageAsync(string deviceGuid, Stream image, CancellationToken cancellationToken);
+}
